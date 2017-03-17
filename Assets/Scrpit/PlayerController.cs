845eaa8a078542832c730ccd5fd8bb1177a56d9a
Tian Tian <tthhss1990@gmail.com>
2017-03-17 00:33:34 -0400

    test2

--- Assets/Scrpit/PlayerController.cs
+++ Assets/Scrpit/PlayerController.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public class Boundary{
+	// comment from other branch
 	public int xMin, xMax, zMin, zMax;
 }
 
