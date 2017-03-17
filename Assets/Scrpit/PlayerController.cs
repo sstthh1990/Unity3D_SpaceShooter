2245da98ccff54496d2b41528a40159e227490e7
Tian Tian <tthhss1990@gmail.com>
2017-03-17 00:00:07 -0400

    init commit

---
+++ Assets/Scrpit/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+	public int xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	public Boundary boundary;
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rb.velocity = new Vector3 (moveHorizontal*speed, 0.0f, moveVertical*speed);
+
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+			
+		rb.rotation = Quaternion.Euler (new Vector3 (0, 0, -moveHorizontal*20));
+	}
+
+}
